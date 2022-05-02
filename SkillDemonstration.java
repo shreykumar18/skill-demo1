badca10c942f0bfd6db15f5c176178782510c9fd
shreykumar18 <s9kumar@ucsd.edu>
2022-05-01 21:34:39 -0700

    Test File

--- SkillDemonstration.java
+++ SkillDemonstration.java
@@ -1,5 +1,7 @@
 public class SkillDemonstration {
 
-
+    public static int multiply(int n, int m) {
+        return n*m;
+    }
 
 }
---
+++ SkillDemonstrationTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemonstrationTest {
+    
+    @Test
+    public void multiplicaiton() {
+        assertEquals(3, SkillDemonstration.multiply(1, 2));
+    }
+
+}
